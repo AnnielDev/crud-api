ea233d7035dcdf6f523d553db2fb53e811da5e7e
Anniel-Reyes <areyes@brisiao.do>
2024-12-11 21:57:21 -0400

    last chages

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
+import cors, { CorsOptions } from "cors";
 import dbConnection from "./database/config";
 import routes from "./routes";
 dotenv.config();
 
 const app: express.Application = express();
+const urlList = ["http://localhost:5173", "https://afrj-crud-operations.netlify.app"];
+const corsOptions: CorsOptions = {
+  origin: function (origin, callback) {
+    if (origin && urlList.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
 
 dbConnection();
 //CORS
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/api", routes);
